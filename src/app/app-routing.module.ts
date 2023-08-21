fa0adb1c25dcca35ae70e53e455bed4ca2688793
Wesley Santos <wvsantos15@gmail.com>
2023-08-21 20:57:34 -0300

    Commit para testar bash

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path : '', component: HomeComponent}
+  { path : '', component: HomeComponent }
 ];
 
 @NgModule({

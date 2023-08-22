2bd8a35f1fa8dc747f75d20449a714ff599c7237
Wesley Santos <wvsantos15@gmail.com>
2023-08-22 13:21:32 -0300

    Model para listagem de cadastros

---
+++ src/app/posto-combustivel.ts
@@ -0,0 +1,10 @@
+export interface PostoCombustivel {
+
+  nome: string;
+  nomePosto: string;
+  tipoCombustivel: string;
+  valorCombustivel: number;
+  endereco: string;
+  formaPagamento: string;
+  imagemPath?: string;
+}

e81487c37c0b0826af2f32268839e427a30c1a7f
Wesley Santos <wvsantos15@gmail.com>
2023-08-22 13:22:42 -0300

    Alteração do form para listagem de cadastros

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HomeService } from '../home.service';
+import { PostoCombustivel } from '../posto-combustivel';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  postosCombustivel: PostoCombustivel[] = [];
+
+  constructor(private homeService: HomeService){}
+
+  ngOnInit(): void {
+    this.carregarPostosCombustivel();
+  }
+
+  carregarPostosCombustivel() {
+    this.homeService.getPostosCombustivel().subscribe(
+      (postos: PostoCombustivel[]) => {
+        this.postosCombustivel = postos;
+      },
+      error => {
+        console.error('Erro ao carregar postos de combustível', error);
+      }
+    );
+  }
 
 }

f1f906a354fd55788bc36c84ce9827d6082a5c22
Wesley Santos <wvsantos15@gmail.com>
2023-08-25 22:27:45 -0300

    Criação da rota para chamar a view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastrarPrecoComponent } from './cadastrar-preco/cadastrar-preco.component';
+import { AddComentarioComponent } from './add-comentario/add-comentario.component';
 
 const routes: Routes = [
   { path : '', component: HomeComponent },
-  { path: 'cadastrar', component: CadastrarPrecoComponent}
+  { path: 'cadastrar', component: CadastrarPrecoComponent},
+  { path: 'addcoment', component: AddComentarioComponent }
 ];
 
 @NgModule({

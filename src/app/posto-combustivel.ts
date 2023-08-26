97b7eeaa2badd6f8e4ca2562944ef72e84aa9626
Wesley Santos <wvsantos15@gmail.com>
2023-08-25 22:07:14 -0300

    Importação de modelo comentários para interação entre tabelas

--- src/app/posto-combustivel.ts
+++ src/app/posto-combustivel.ts
@@ -1,5 +1,8 @@
+import { Comentario } from "./comentario";
+
 export interface PostoCombustivel {
 
+  id: number;
   nome: string;
   nomePosto: string;
   tipoCombustivel: string;
@@ -7,4 +10,6 @@ export interface PostoCombustivel {
   endereco: string;
   formaPagamento: string;
   imagemPath?: string;
+
+  comentarios: Comentario[]; // Adicione a propriedade comentarios
 }

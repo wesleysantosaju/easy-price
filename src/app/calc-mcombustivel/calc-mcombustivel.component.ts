a204fafc2062c418e1525e411fc5a6d50f910b29
Wesley Santos <wvsantos15@gmail.com>
2023-08-27 13:44:53 -0300

    Configuração da lógica para calcular melhor combustível

--- src/app/calc-mcombustivel/calc-mcombustivel.component.ts
+++ src/app/calc-mcombustivel/calc-mcombustivel.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-calc-mcombustivel',
   templateUrl: './calc-mcombustivel.component.html',
   styleUrls: ['./calc-mcombustivel.component.scss']
 })
-export class CalcMcombustivelComponent {
+export class CalcMcombustivelComponent implements OnInit {
+  form!: FormGroup;
+  resultadoCalculo: string | null = null;
+
+  constructor(private formBuilder: FormBuilder){}
+
+
+  ngOnInit() {
+    this.form = this.formBuilder.group({
+      valorAlcool: '',
+      valorGasolina: ''
+    });
+  }
+
+  calcularCusto() {
+    const valorAlcool = this.form.value.valorAlcool;
+    const valorGasolina = this.form.value.valorGasolina;
+
+    if (valorAlcool && valorGasolina) {
+      const result = valorAlcool / valorGasolina;
+
+      if (result <= 0.7) {
+        this.resultadoCalculo = "Recomendado o abastecimento com álcool!";
+      } else {
+        this.resultadoCalculo = "Recomendado o abastecimento com gasolina!";
+      }
+    }
+  }
 
 }

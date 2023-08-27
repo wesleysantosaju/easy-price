79b869489bf808735619f84c5a9ca536b85e1c77
Wesley Santos <wvsantos15@gmail.com>
2023-08-27 13:21:56 -0300

    Configuração de snackbar ao cadastrar itens para melhor interação com o usuário

--- src/app/add-comentario/add-comentario.component.ts
+++ src/app/add-comentario/add-comentario.component.ts
@@ -1,8 +1,9 @@
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { HomeService } from './../home.service';
 import { Comentario } from '../comentario';
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, ViewChild } from '@angular/core';
 import { PostoCombustivel } from '../posto-combustivel';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 @Component({
@@ -13,11 +14,13 @@ import { ActivatedRoute, ParamMap } from '@angular/router';
 export class AddComentarioComponent {
   comentarioForm: FormGroup;
   postoId: number | null = null; // Propriedade para armazenar o postoId
+  @ViewChild('snackbar', { static: true }) snackbar: any; // Referência para o elemento snackbar
 
   constructor(
     private formBuilder: FormBuilder,
     private comentarioService: HomeService,
     private route: ActivatedRoute,
+    private snackBar: MatSnackBar,
     @Inject(MAT_DIALOG_DATA) public data: any
   ) {
     this.comentarioForm = this.formBuilder.group({
@@ -45,12 +48,37 @@ export class AddComentarioComponent {
 
     this.comentarioService.cadastrarComentario(novoComentario).subscribe(
       (response) => {
-        // Lógica de sucesso, se necessário
+        const snackBarRef = this.exibirSnackbar('Comentário realizado com sucesso');
+
+        // Fechar o diálogo e recarregar a página após o fechamento da snackbar
+        snackBarRef.afterDismissed().subscribe(() => {
+          this.fecharFormulario();
+        });
       },
       (error) => {
-        // Lógica de erro, se necessário
+        // Exibir a snackbar e executar ações após o fechamento
+        const snackBarRef = this.exibirSnackbar('Erro ao inserir comentário');
+
+        // Fechar o diálogo e recarregar a página após o fechamento da snackbar
+        snackBarRef.afterDismissed().subscribe(() => {
+          this.fecharFormulario();
+        });
       }
     );
+  }
+
+  exibirSnackbar(mensagem: string) {
+    return this.snackBar.open(mensagem, 'Fechar', {
+      duration: 0, // Manter a snackbar aberta
+    });
+  }
+  fecharFormulario() {// Esconder o snackbar ou diálogo, se estiver aberto
+    this.snackBar.dismiss(); // Se estiver usando o MatSnackBar
+
+    // Recarregar a página após alguns milissegundos
+    setTimeout(() => {
+      window.location.reload();
+    }, 1000); // Aguarde 1 segundo antes de recarregar
   }
 
   ngOnInit(): void {
@@ -64,6 +92,7 @@ export class AddComentarioComponent {
       }
     });
 
-}
+  }
+
 
 }
--- src/app/cadastrar-preco/cadastrar-preco.component.ts
+++ src/app/cadastrar-preco/cadastrar-preco.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { HomeService } from '../home.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-cadastrar-preco',
@@ -9,12 +10,15 @@ import { HomeService } from '../home.service';
 })
 export class CadastrarPrecoComponent {
 
+  @ViewChild('snackbar', { static: true }) snackbar: any; // Referência para o elemento snackbar
+
   form: FormGroup;
   selectedImage: File | null = null;
 
   selectedFileName: string = '';
+  exibirFormulario: boolean = true;
 
-  constructor(private formBuilder: FormBuilder, private home: HomeService) {
+  constructor(private formBuilder: FormBuilder, private home: HomeService, private snackBar: MatSnackBar) {
     this.form = this.formBuilder.group({
       nome: '',
       nomePosto: '',
@@ -40,25 +44,61 @@ export class CadastrarPrecoComponent {
     formData.append('formaPagamento', this.form.value.formaPagamento);
 
     if (this.selectedImage !== null) {
+      // Se uma imagem foi selecionada, anexá-la ao formData
       formData.append('image', this.selectedImage);
     }
 
-    this.home.cadastrarPosto(formData).subscribe(
-      response => {
-        console.log('Posto cadastrado com sucesso', response);
-        // Limpar o formulário ou fazer outras ações após o cadastro
+    // Chamar o serviço para cadastrar o posto com os dados do formData
+    this.home.cadastrarPosto(formData).subscribe({
+      next: response => {
+        console.log('Resposta do servidor:', response);
+
+        // Exibir a snackbar e executar ações após o fechamento
+        const snackBarRef = this.exibirSnackbar('Cadastro realizado com sucesso');
+
+        // Fechar o diálogo e recarregar a página após o fechamento da snackbar
+        snackBarRef.afterDismissed().subscribe(() => {
+          this.fecharFormulario();
+        });
       },
-      error => {
-        console.error('Erro ao cadastrar posto', error);
+      error: error => {
+        console.error('Erro:', error);
+
+        // Exibir a snackbar e executar ações após o fechamento
+        const snackBarRef = this.exibirSnackbar('Cadastro realizado com sucesso');
+
+        // Fechar o diálogo e recarregar a página após o fechamento da snackbar
+        snackBarRef.afterDismissed().subscribe(() => {
+          this.fecharFormulario();
+        });
       }
-    );
+    });
+  }
+
+  exibirSnackbar(mensagem: string) {
+    return this.snackBar.open(mensagem, 'Fechar', {
+      duration: 0, // Manter a snackbar aberta
+    });
+  }
+fecharFormulario() {// Esconder o snackbar ou diálogo, se estiver aberto
+  this.snackBar.dismiss(); // Se estiver usando o MatSnackBar
+
+  // Recarregar a página após alguns milissegundos
+  setTimeout(() => {
+    window.location.reload();
+  }, 1000); // Aguarde 1 segundo antes de recarregar
+}
+
+  limparFormulario() {
+    this.form.reset(); // Isso irá redefinir todos os campos do formulário para os valores padrão
+    this.selectedImage = null; // Limpar a imagem selecionada, se houver
   }
 
   handleImageInput(event: any) {
     const file = event.target.files[0];
 
     if (file) {
-      // Aqui você pode implementar a lógica para processar o arquivo de imagem, como fazer upload para um servidor.
+      // Aqui a lógica para processar o arquivo de imagem, como fazer upload para um servidor.
       this.selectedFileName = file.name;
       console.log('Imagem selecionada:', file);
     }
